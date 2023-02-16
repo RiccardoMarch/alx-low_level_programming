873caf80ccc73779f4ca79b2243e333a9281c189
yikes <sikkid47@gmail.com>
2023-02-16 11:37:47 +0200

    fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
-*main-Entry point
-*Return:An Integer of 0 Success in execution
-*/
+/**
+ *main-Entry point
+ *
+ *Return:An Integer of 0 Success in execution
+ */
 
 int main(void) 
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	    return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
