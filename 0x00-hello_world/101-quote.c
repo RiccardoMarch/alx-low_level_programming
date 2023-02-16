e46dd68fbc296089f7dfb070ac02415d24312d32
yikes <sikkid47@gmail.com>
2023-02-16 12:17:09 +0200

    initial commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Return: An Integer of 0 Success in execution
+ */
+
+int main(void)
+{
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+return (stdout, text, strlen(text));
+}
