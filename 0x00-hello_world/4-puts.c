c14f76332b64255124efd3f268a430700f43cf2f
yikes <sikkid47@gmail.com>
2023-02-16 11:29:42 +0200

    initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+* Entry point - main
+* Returns an integar : 0 Success in execution
+*/
+
+int main (void) {
+    puts('"Programming is like building a multilingual puzzle');
+    return 0;
+}
