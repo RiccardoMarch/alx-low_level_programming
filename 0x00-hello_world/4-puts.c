b3d71b558df3db31e35e69da0697d6dc7a3b455a
yikes <sikkid47@gmail.com>
2023-02-16 11:38:59 +0200

    fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  *Return:An Integer of 0 Success in execution
  */
 
-int main(void) 
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
