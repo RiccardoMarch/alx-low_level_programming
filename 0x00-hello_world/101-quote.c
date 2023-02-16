d905a701d3981a70473e6dc079c27fb574e99306
yikes <sikkid47@gmail.com>
2023-02-16 12:21:42 +0200

    fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, text, strlen(text));
-return (1)
+return (1);
 }
