ea757f3dd6eaae9642b875951a0ff4149e93c88d
yikes <sikkid47@gmail.com>
2023-02-17 13:21:07 +0200

    Fixes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n;
 
-	char output[] = n >= 0 ? "positive" : "negative";
+	char *output = (n >= 0) ? "positive" : "negative";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
