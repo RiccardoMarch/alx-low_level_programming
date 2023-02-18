a0f529c9e8c85f3304c991af3c72d3095554d664
yikes <sikkid47@gmail.com>
2023-02-18 05:21:00 +0200

    initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: output a-z alphabet and new line
+ *
+ *Return: An Integer of 0 Success in execution
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	do {
+	if (alpha == 'q' || alpha == 'e')
+	continue;
+	putchar(alpha);
+	alpha++;
+	} while (alpha <= 'z');
+	putchar('\n');
+	return (0);
+}
