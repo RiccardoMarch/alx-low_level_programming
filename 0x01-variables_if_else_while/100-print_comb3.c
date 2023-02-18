472c762742edc9149413f71a1d487934fc1b2c69
yikes <sikkid47@gmail.com>
2023-02-18 05:49:53 +0200

    initial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: 
+ *
+ *Return: An Integer of 0 Success in execution
+ */
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = 0; tens <= 9; tens++) {
+		for (ones = 0; ones <= 9; ones++) {
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != 8 || ones != 9) {
+				putchar(',');
+				putchar(' ');
+            }
+        }
+	}
+
+	putchar('\n');
+	return (0);
+}
