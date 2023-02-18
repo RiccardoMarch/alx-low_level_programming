fe28932d26a2a4a0f230d836c0b2034ededc5a9a
yikes <sikkid47@gmail.com>
2023-02-18 05:28:36 +0200

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: output 0-9 numbers and new line
+ *
+ *Return: An Integer of 0 Success in execution
+ */
+int main(void)
+{
+	int num = '0';
+
+	do {
+		putchar(num);
+		num++;
+	} while (num < 10);
+
+	putchar('\n');
+	return (0);
+}
