a1a4eafa7628bd72232b4af196febbccc2703209
yikes <sikkid47@gmail.com>
2023-02-18 05:05:58 +0200

    Betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,3 @@
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -7,21 +5,23 @@
 /**
  *main-Entry point
  *
- *Description: Check if the last digit of an int is greater then 5, less or equal to zero
+ *Description: validate last digit in a integer
  *
  *Return: An Integer of 1 Success in execution
  */
 int main(void)
 {
 	int n;
-    int l_digit;
+	int l_digit;
 	char *output;
+	char greater[] = "greater than 5";
+	char less[] = "less than 6 and not 0";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    l_digit = n % 10;
+	l_digit = n % 10;
 	/* Used tenary method to set value to the string variable */
-	output = (l_digit <= 5) ? (l_digit == 0) ? "0" : "less than 6 and not 0" : "greater than 5";
+	output = (l_digit <= 5) ? (l_digit == 0) ? "0" : less : greater;
 	printf("Last digit of %d is %d and is %s\n", n, l_digit, output);
 	return (0);
 }
