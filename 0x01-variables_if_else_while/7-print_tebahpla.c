2504df686d4380e04947a066911b30e67af16910
yikes <sikkid47@gmail.com>
2023-02-18 05:30:59 +0200

    initial commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: output a-z alphabet in reverse and new line
+ *
+ *Return: An Integer of 0 Success in execution
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	do {
+	putchar(alpha);
+	alpha--;
+	} while (alpha >= 'a');
+	putchar('\n');
+	return (0);
+}
