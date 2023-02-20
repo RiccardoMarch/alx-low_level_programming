2cbcd4c6cedc7191dcbc5de7b1cec8b7d631522c
yikes <sikkid47@gmail.com>
2023-02-20 17:45:51 +0200

    fixes

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main-Entry point
+ *
+ *Description:
+ *
+ *Return: An Integer of 0 Success in execution
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int offset = '0';
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if (a == b || b == c || c == a || a > b || b > c || a > c)
+					continue;
+
+				putchar(offset + a);
+				putchar(offset + b);
+				putchar(offset + c);
+
+				if ((a + b + c) >= 24)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
