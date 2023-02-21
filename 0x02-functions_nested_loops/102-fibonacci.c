0eb58e9de985fb3a96bba51672e218fe3de4c783
yikes <sikkid47@gmail.com>
2023-02-22 01:47:19 +0200

    fixes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i, a = 1, b = 2;
-    int result[50];
+    long int result[50];
 
     result[0] = a;
     result[1] = b;
@@ -16,7 +16,7 @@ int main(void)
     for (i = 2; i < 50; i++)
     {
         result[i] = result[i - 1] + result[i - 2];
-        printf("%d%s", result[i], (i == 59) ? "\n" : ", ");
+        printf("%ld%s", result[i], (i == 59) ? "\n" : ", ");
     }
 	return (0);
 }
