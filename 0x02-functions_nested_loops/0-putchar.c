15bba9a45410152b2926e8479451b41ab15e6d4e
yikes <sikkid47@gmail.com>
2023-02-21 16:50:13 +0200

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: Check if an n is positive, zero or negative
+ *
+ *Return: An Integer of 1 Success in execution
+ */
+int main(void)
+{
+	int i;
+	char *output = "_putchar";
+
+	for (i = 0; i < strlen(output); i++)
+	{
+		putchar(output[i]);
+	}
+	putchar('\n');
+	return (0);
+}
