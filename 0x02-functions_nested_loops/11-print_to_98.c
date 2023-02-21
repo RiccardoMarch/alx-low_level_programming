a6fe66c36083ae3f71e66625a7355051122ae3e6
yikes <sikkid47@gmail.com>
2023-02-22 00:59:43 +0200

    initial commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - void
+* Description: prints from @a till 98
+* @n: value to start from
+* Return: None
+*/
+
+void print_to_98(int n)
+{
+	int x;
+	for(x = n; x != 98; (n > 98) ? x-- : x++)
+	{
+		printf("%d,", x);
+	}
+
+	printf("98\n");
+}
