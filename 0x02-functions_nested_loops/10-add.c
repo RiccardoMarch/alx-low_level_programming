6a9408f9f4bae88b6abbc4af07adf3ce30f977ed
yikes <sikkid47@gmail.com>
2023-02-22 00:51:48 +0200

    fixes

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,9 @@
 /**
 * add - void
 * Description: Sum up the value of a & b
-* Return: value of (a + b)
+* @a: value of a
+* @b: value of b
+* Return: value of ( @a + @b )
 */
 int add(int a, int b)
 {
