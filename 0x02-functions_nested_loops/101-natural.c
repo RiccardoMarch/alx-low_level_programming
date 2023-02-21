838cd5b9d6e726daa19c70bf2b49b9049b91433d
yikes <sikkid47@gmail.com>
2023-02-22 01:36:53 +0200

    initial commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i, result = 0;
+
+	for (i = 0; i < 1024; i++)
+		result += ((i % 5) == 0 || (i % 3) == 0) ? i : 0;
+	
+	printf("%d\n", result);
+}
