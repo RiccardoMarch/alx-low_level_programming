448fbfdb8adb2680d7619ed0e32b48578d61ca90
yikes <sikkid47@gmail.com>
2023-02-23 10:08:55 +0200

    fixes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,11 +12,18 @@
 
  void print_remaining_days(int month, int day, int year)
  {
-	bool is_leap_year;
-	int total_days, days_passed, i;
+	bool is_leap_year = ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0) ? true : false;
+	int total_days = (is_leap_year) ? 366: 365;
+	int days_passed = day;
+	int i;
 
+	for (i = 1; i < month; i++) {
+		if (i == 1)
+			days_passed += (is_leap_year) ? 29 : 28;
+		else
+			days_passed += (((i + 1) % 2) == 0) ? 30 : 31;
+	}
 
-
-	printf("Day of the year: %d\n", days_passed);
-	printf("Remaining days: %d\n", (total_days - days_passed));
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", (total_days - day));
  }
