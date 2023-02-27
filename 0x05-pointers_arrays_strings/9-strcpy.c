f0c021791bbcd49f0b4150ad2a6230146abeba5a
RiccardoMarch <sikkid47@gmail.com>
2023-02-27 22:44:48 +0200

    fixed betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,15 +11,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	do
-	{
+	do {
 		dest[i] = src[i];
 		if (src[i] == '\0')
 			break;
 
 		i++;
-	}
-	while (i >= 0);
+	} while (i >= 0);
 
 	return (dest);
 }
