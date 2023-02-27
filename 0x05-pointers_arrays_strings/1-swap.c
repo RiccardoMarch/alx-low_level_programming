99a01eed843c39e4878fba5ed7efa179396738ce
RiccardoMarch <sikkid47@gmail.com>
2023-02-27 21:04:07 +0200

    initial commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+* swap_int - it swaps the value of the passed int args
+* @a: pointer value to swap with @b
+* @b: pointer value to swap with @a
+* Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int holder;
+
+	holder = *a;
+	a* = *b;
+	b* = holder;
+}
