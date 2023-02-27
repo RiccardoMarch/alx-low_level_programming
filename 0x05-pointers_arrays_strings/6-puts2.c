39265c38347e508126688f069b71030aeab39a09
RiccardoMarch <sikkid47@gmail.com>
2023-02-27 22:13:33 +0200

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; (str[i] != '\0') && (i % 2 == 0); i++)
-		_putchar(str[i]);
+	for (i = 0; (str[i] != '\0'); i++)
+		_putchar((i % 2 == 0) ? str[i] : '');
 
 	_putchar('\n');
 }
