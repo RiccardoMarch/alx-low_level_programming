445bed49b573690824f0aed9fff37f0c42350089
RiccardoMarch <sikkid47@gmail.com>
2023-02-27 20:55:08 +0200

    initial commit

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * reset_to_98 - resets passed argument to 98
+ * @n: pointer value to reset
+ * Return: nothing
+ */
+
+ void reset_to_98(int *n)
+ {
+	int *ptr;
+
+	ptr = n;
+
+	*ptr = 98;
+ }
