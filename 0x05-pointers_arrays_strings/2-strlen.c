e93b3c90ccc14544fd6c8ae4354bca61273a3265
RiccardoMarch <sikkid47@gmail.com>
2023-02-27 21:10:46 +0200

    initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+* _strlen - it calculates the length of a char pointers
+* @s: pointer to a string
+* Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+
+	return (i);
+}
