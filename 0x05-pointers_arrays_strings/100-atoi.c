f149efe9ba0d19366a9a76561e25ead416135e5d
RiccardoMarch <sikkid47@gmail.com>
2023-02-27 23:12:08 +0200

    fixes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,21 +11,21 @@ int _atoi(char *str)
 	int i = 0, length = 0, s = 1, result = 0;
 
 	do {
-		if (length > 0 && (s[i] > '9' || s[i] < '0'))
+		if (length > 0 && (str[i] > '9' || str[i] < '0'))
 			break;
 
-		s *= (s[i] == '-') -1 : 1;
+		s *= (str[i] == '-') -1 : 1;
 
-		if (s[i] >= '0' && s[i] <= '9') {
+		if (str[i] >= '0' && str[i] <= '9') {
 			if (result > 0)
 				result *= 10;
 
-			result += *(s + i);
+			result += *(str + i);
 			length++;
 		}
 
 		i++;
-	} while (s[i] != '\0');
+	} while (str[i] != '\0');
 
 	return ((result == 0) ? 0 : (result * s));
 }
