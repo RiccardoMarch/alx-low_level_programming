0ad710f06b6622ca0f6061772d20e9f04c832406
RiccardoMarch <sikkid47@gmail.com>
2023-03-01 18:20:07 +0200

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,8 +9,8 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int offset = 2;
-	int i = 1;
+	int offset = 0;
+	int i = 0;
 
 	do {
 		offset++;
@@ -19,6 +19,6 @@ char *_strcat(char *dest, char *src)
 	for (; src[i] != '\0'; i++, offset++)
 		dest[offset] = src[i];
 
-	dest[i] = '\0';
+	dest[offset] = '\0';
 	return (dest);
 }
