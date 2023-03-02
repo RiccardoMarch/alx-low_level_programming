59c376973c5c98ce3659373f8b3e1533d71e4fe9
RiccardoMarch <sikkid47@gmail.com>
2023-03-02 12:01:41 +0200

    initial commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* reverse_array - reverse an integer array
+* @array: pointer to integer array
+* @n: size of array
+* Return: Nothing
+*/
+
+void reverse_array(int *array, int n)
+{
+	int i = 0;
+	int temp;
+
+	n = n - 1;
+
+	do {
+		temp = array[i];
+		array[i] = a[n];
+		array[n] = temp;
+		i++;
+		n--;
+	} while (i < (n / 2));
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *a, char *b);
 
+void reverse_array(int *array, int n);
+
 #endif
