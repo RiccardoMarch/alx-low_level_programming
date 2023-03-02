ad6407a4640d2c369301dc008d200bb0c4d70ba4
RiccardoMarch <sikkid47@gmail.com>
2023-03-02 10:30:21 +0200

    simplified while loop

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,8 @@ char *_strncat(char *dest, char *src, int n)
 	int offset = 0;
 	int i = 0;
 
-	do {
-		offset++;
-	} while (dest[offset] != '\0');
+	while (dest[offset] != '\0')
+        offset++;
 
 	do {
 		dest[offset] = src[i];
