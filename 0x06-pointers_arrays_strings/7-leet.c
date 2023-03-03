a230bfcf70f08cbd4331095d8ccd898267e926ee
RiccardoMarch <sikkid47@gmail.com>
2023-03-03 12:10:20 +0200

    Initial commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* *leet - Encodes a string into 1337
+* @s: pointer to string
+* Return: the pointer to string
+*/
+
+char *leet(char *s)
+{
+	int length = 0;
+	int index;
+	int temp;
+
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	for (; s[length] != '\0')
+	{
+		for (index = 0; index < 5; index++)
+		{
+			temp = s[length];
+			if(temp == low_letters[i] || temp == upp_letters[i])
+			{
+				s[length] = numbers[i];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
