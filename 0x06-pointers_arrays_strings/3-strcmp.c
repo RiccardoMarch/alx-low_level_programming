201bfb07caaa9a5e18bf5e7f613f49cba8cf1208
RiccardoMarch <sikkid47@gmail.com>
2023-03-03 12:37:24 +0200

    Fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,8 @@ int _strcmp(char *a, char *b)
 	i = 0;
 	result = 0;
 
-	while (result == 0)
-	{
-		if ((a[i] == '\0') && (b[i] == '\0'))
-			break;
-
+	for (i = 0; (a[i] != '\0') && (result == 0); i++)
 		result = (a[i] - b[i]);
-		i++;
-	}
 
 	return (result);
 }
